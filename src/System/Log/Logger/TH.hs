5bb75a633758bca4ea33c635cfc8db9594ecbdeb
Brian Lewis <brian@lorf.org>
2009-05-21 19:08:57 -0500

    Minor comment formatting.

--- src/System/Log/Logger/TH.hs
+++ src/System/Log/Logger/TH.hs
@@ -1,49 +1,53 @@
--- | This module provides functions that generate hslogger functions using
--- Template Haskell.
+{-|
+  This module provides functions that generate hslogger functions using
+  Template Haskell.
+-}
 module System.Log.Logger.TH (deriveLoggers) where
 
 import qualified Language.Haskell.TH as TH
 
 import qualified System.Log.Logger as HSL
 
--- | Generate hslogger functions for a list of priorities.
---
--- Example usage:
---
--- > module Foo.Bar ( ... ) where
--- >
--- > import System.Log.Logger.TH (deriveLoggers)
--- > import qualified System.Log.Logger as HSL
--- >
--- > $(deriveLoggers "HSL" [HSL.DEBUG, HSL.INFO])
---
--- Used this way, @deriveLoggers@ would generate the following functions:
---
--- > infoM :: String -> IO ()
--- > infoM s = HSL.infoM "Foo.Bar" ((++) "Foo.Bar: " s)
--- >
--- > debugM :: String -> IO ()
--- > debugM s = HSL.debugM "Foo.Bar" ((++) "Foo.Bar: " s)
---
--- The other hslogger priorities follow the same pattern.
---
--- So
---
--- > infoM "hi there"
---
--- would generate the INFO-level log event
---
--- > Foo.Bar: hi there
---
--- Notes:
---
---   * "System.Log.Logger" must be imported qualified, and the qualifier must
---   match the qualifier given to @deriveLoggers@.
---
---   * Don't forget to enable Template Haskell preprocessing: specify the
---   pragma @LANGUAGE TemplateHaskell@ at the top of your source file or
---   @extensions: TemplateHaskell@ in your cabal file, etc.
---
+{-|
+  Generate hslogger functions for a list of priorities.
+
+  Example usage:
+
+  > module Foo.Bar ( ... ) where
+  >
+  > import System.Log.Logger.TH (deriveLoggers)
+  > import qualified System.Log.Logger as HSL
+  >
+  > $(deriveLoggers "HSL" [HSL.DEBUG, HSL.INFO])
+
+  Used this way, @deriveLoggers@ would generate the following functions:
+
+  > infoM :: String -> IO ()
+  > infoM s = HSL.infoM "Foo.Bar" ((++) "Foo.Bar: " s)
+  >
+  > debugM :: String -> IO ()
+  > debugM s = HSL.debugM "Foo.Bar" ((++) "Foo.Bar: " s)
+
+  The other hslogger priorities follow the same pattern.
+
+  So
+
+  > infoM "hi there"
+
+  would generate the INFO-level log event
+
+  > Foo.Bar: hi there
+
+  Notes:
+
+    * "System.Log.Logger" must be imported qualified, and the qualifier must
+    match the qualifier given to @deriveLoggers@.
+
+    * Don't forget to enable Template Haskell preprocessing: specify the
+    pragma @LANGUAGE TemplateHaskell@ at the top of your source file or
+    @extensions: TemplateHaskell@ in your cabal file, etc.
+-}
+
 deriveLoggers
   :: String          -- ^ Must match qualifier on import of "System.Log.Logger".
   -> [HSL.Priority]  -- ^ List of priorities for which to generate logging functions.
