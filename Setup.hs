f284bb3cc9da957be9f1c295f21c303c6888839c
Brian Lewis <brian@lorf.org>
2010-08-25 21:08:10 -0400

    Cause 'cabal test' to run new testNamed.hs

--- Setup.hs
+++ Setup.hs
@@ -8,6 +8,11 @@ main :: IO ()
 main =
     defaultMainWithHooks $ simpleUserHooks { runTests = runTests' }
   where
-    runTests' _ _ _ _ = do
-        system "runhaskell -i./src src/test.hs"
-        return ()
+    runTests' _ _ _ _ =
+        mapM_ runTest ["test.hs", "testNamed.hs"]
+      where
+        runTest fp = do
+            putStrLn $ fp ++ ":"
+            system $ "runhaskell -i./src src/" ++ fp
+            putStrLn ""
+            return ()
